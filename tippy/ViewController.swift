61e1b58f8875332d3630ee456185b7409afc26ab
sabrina-git <sabrina-git@users.noreply.github.com>
2021-01-08 18:21:26 -0800

    connected app icons to code

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        
+        view.endEditing(true)
+    }
+ 
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        // get bill amt
+        
+        // calc tip and total
+        
+        // update tip and total labels
+        
+    }
+    
+    
 }
 
